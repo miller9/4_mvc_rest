95429d017981a5354359ebb5780b84563aa7e074
miller9 <millerpoli9@gmail.com>
2020-04-16 23:12:09 -0400

    modificar archivo SEED para ingresar datos a BDD

--- seeders/20200417025208-generate_tasks.js
+++ seeders/20200417025208-generate_tasks.js
@@ -2,25 +2,14 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('tasks', [
+      {id: 1, description: 'Grabar curso de backend', createdAt: new Date(), updatedAt: new Date()},
+      {id: 2, description: 'Editar los videos del curso', createdAt: new Date(), updatedAt: new Date()},
+      {id: 3, description: 'Subir los videos a plataforma', createdAt: new Date(), updatedAt: new Date()}
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('tasks', null, {});
   }
 };
