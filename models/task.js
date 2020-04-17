69c2a530f32258c4e6a34821fb91179110b7dfc2
miller9 <millerpoli9@gmail.com>
2020-04-16 21:41:06 -0400

    ELiminando Asociaciones del Modelo Task. Eliminando objeto <sequelize> de server.js

--- models/task.js
+++ models/task.js
@@ -3,8 +3,6 @@ module.exports = (sequelize, DataTypes) => {
   const Task = sequelize.define('Task', {
     description: DataTypes.TEXT
   }, {});
-  Task.associate = function(models) {
-    // associations can be defined here
-  };
+
   return Task;
 };
