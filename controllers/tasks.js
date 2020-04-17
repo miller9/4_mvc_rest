4bedf2c3ad06d483e4547e5f9d74d5f58f4d1572
miller9 <millerpoli9@gmail.com>
2020-04-17 14:37:45 -0400

    Actualizando archivo de rutas <tasks_routes.js>, controlador <tasks.js> y configurar servidor <server.js>

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,9 +2,14 @@ const Task = require('../models').Task; // import carpeta models (importar MODEL
 
 
 module.exports = {
-  home: function(req,res){
-    Task.findAll().then(function(tasks){
-      res.render('tasks/index',{tasks: tasks});
+  create: function(req,res){
+    Task.create({
+      description: req.body.description
+    }).then(result=>{
+      res.json(result);
+    }).catch(err=>{
+      console.log(err);
+      res.json(err);
     })
   }
-}
+};
--- routes/tasks_routes.js
+++ routes/tasks_routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
+// importar controlador para definir funcionalidad de cada ruta
+let TasksController = require('../controllers/tasks');
 // llamar metodo 'Router' de express. El objeto 'router' define las rutas..
 let router = express.Router();
 
 // Definicion de rutas
 router.route('/tasks').get(function(req,res){
   res.send('Hello world desde una subruta');
-}).post(function(req,res){
+}).post(TasksController.create);
 
-});
 // montar grupo de rutas en el servidor principal
 module.exports = router;
