ef20b8b76cc7a28c437057ccf99749273a179aad
miller9 <millerpoli9@gmail.com>
2020-04-18 12:50:45 -0400

    CERRAR SESION: Crear ruta en <sessions_routes.js> para ELIMINAR sesion. Incluir metodo DESTROY en controlador <sessions.js>. -Actualizar HOME para incluir boton de cerrar sesion

--- routes/sessions_routes.js
+++ routes/sessions_routes.js
@@ -5,6 +5,9 @@ let SessionsController = require('../controllers/sessions');
 
 let router = express.Router();
 
-router.route('/sessions').get(SessionsController.new).post(SessionsController.create);
+router.route('/sessions')
+  .get(SessionsController.new)
+  .post(SessionsController.create)
+  .delete(SessionsController.destroy);
 
 module.exports = router;
